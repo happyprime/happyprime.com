1ebbaa5906644325e7724cbc2fc7cb424d361ad8
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-18 14:38:18 -0600

    Add a Google Analytics tracking script in the footer

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ namespace HappyPrime\Theme;
 
 add_action( 'wp_enqueue_scripts', 'HappyPrime\Theme\enqueue_styles' );
 add_action( 'init', 'HappyPrime\Theme\add_shortcode' );
+add_action( 'wp_footer', 'HappyPrime\Theme\analytics_footer' );
 
 /**
  * Enqueue the default styles provided by the parent theme.
@@ -45,4 +46,24 @@ function logo_svg() {
 	ob_end_clean();
 
 	return $content;
+}
+
+/**
+ * Output the Happy Prime GA tag in the footer.
+ *
+ * @since 0.0.2
+ */
+function analytics_footer() {
+	?>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-104910839-1', 'auto');
+		ga('send', 'pageview');
+
+	</script>
+	<?php
 }
